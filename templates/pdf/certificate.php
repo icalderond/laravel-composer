f0305e24fbc2282cf144f86f1ebb1260110287f5
icalderond <icalderond@outlook.com>
2015-03-03 17:13:25 -0600

    Subida desde ericher

---
+++ templates/pdf/certificate.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Curso de laravel</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href="../../assets/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>  
+	<p class="logo">
+	<img src="../../assets/Certificado_QR.png" width="200">
+	</p>
+	<p>Por haber aprobado</p>
+	<h1><?= $name ?></h1>
+</body>
+</html>
